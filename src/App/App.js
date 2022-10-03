ab472865880b1b5405116509096db617ba7c1995
wildchamo <wildchamo@gmail.com>
2022-10-03 18:10:09 -0500

    base componente APPUI para imprimir los proyectos

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { AppUI } from './AppUI.js';
 
 const defaultProyectos = [
-  { nombre: 'ChatBot Sinapsis (Diseño de flujo)', fechaInicio:'' , fechaFin:'',fechaCreación:'',estado:'Activo', descripcion:'Soy una descripción' },
-  { nombre: 'Investigación arquitectura PWA', fechaInicio:'' , fechaFin:'',fechaCreación:'',estado:'Cerrado', descripcion:'Soy una descripción' },
-  { nombre: 'Software de encuestas Limesurvey', fechaInicio:'' , fechaFin:'',fechaCreación:'',estado:'Cancelado', descripcion:'Soy una descripción' }
+  { nombre: 'ChatBot Sinapsis (Diseño de flujo)', fechaInicio:'' , fechaFin:'',fechaCreacion:'',estado:'Activo', descripcion:'Soy una descripción' },
+  { nombre: 'Investigación arquitectura PWA', fechaInicio:'' , fechaFin:'',fechaCreacion:'',estado:'Cerrado', descripcion:'Soy una descripción' },
+  { nombre: 'Software de encuestas Limesurvey', fechaInicio:'' , fechaFin:'',fechaCreacion:'',estado:'Cancelado', descripcion:'Soy una descripción' }
 ];
 
 function App() {
+  
   return (
     <AppUI
+    proyectosTotal={defaultProyectos}
     />
   );
 }
--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,1 +1,30 @@
 import React from 'react';
+import {listaProyectoOperativo} from '../Components/listaProyectoOperativo/listaProyectoOperativo';
+import {proyectoOperativo} from '../Components/proyectoOperativo/proyectoOperativo';
+
+
+function AppUI({
+    proyectosTotal,
+  }) 
+  {
+    return (
+      <React.Fragment>
+         
+        <listaProyectoOperativo>
+          {proyectosTotal.map(proyecto => (
+            <proyectoOperativo
+              key={proyecto.nombre}
+              nombre={proyecto.nombre}
+              fechaInicio={proyecto.fechaInicio}
+              fechaFin={proyecto.fechaFin}
+              fechaCreación={proyecto.fechaCreacion}
+              estado={proyecto.estado}
+              descripción= {proyecto.descripción}
+              />
+          ))}
+        </listaProyectoOperativo>  
+      </React.Fragment>
+    );
+  }
+  
+  export { AppUI };
