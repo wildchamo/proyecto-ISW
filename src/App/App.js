a5039151c384963a3d369e00a155c30ce8e347c6
wildchamo <wildchamo@gmail.com>
2022-10-03 19:55:13 -0500

    ya funciona el buscador! Soy un genio

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import { AppUI } from './AppUI.js';
+import React from "react";
+import { AppUI } from "./AppUI.js";
 
 const defaultProyectos = [
-  { nombre: 'ChatBot Sinapsis (Diseño de flujo)', fechaInicio:'2021-11-03' , fechaFin:'2022-10-02',fechaCreacion:'2021-11-03',estado:'Activo', descripcion:'Soy una descripción' },
-  { nombre: 'Investigación arquitectura PWA', fechaInicio:'2021-11-03' , fechaFin:'2022-10-02',fechaCreacion:'',estado:'Cerrado', descripcion:'Soy una descripción' },
-  { nombre: 'Software de encuestas Limesurvey', fechaInicio:'2021-11-03' , fechaFin:'2022-10-02',fechaCreacion:'',estado:'Cancelado', descripcion:'Soy una descripción' }
+  {
+    nombre: "ChatBot Sinapsis (Diseño de flujo)",
+    fechaInicio: "2021-11-03",
+    fechaFin: "2022-10-02",
+    fechaCreacion: "2021-11-03",
+    estado: "Activo",
+    descripcion: "Soy una descripción",
+  },
+  {
+    nombre: "Investigación arquitectura PWA",
+    fechaInicio: "2021-11-03",
+    fechaFin: "2022-10-02",
+    fechaCreacion: "",
+    estado: "Cerrado",
+    descripcion: "Soy una descripción",
+  },
+  {
+    nombre: "Software de encuestas Limesurvey",
+    fechaInicio: "2021-11-03",
+    fechaFin: "2022-10-02",
+    fechaCreacion: "",
+    estado: "Cancelado",
+    descripcion: "Soy una descripción",
+  },
 ];
 
 function App() {
-  
+  const [proyectos, setProyectos] = React.useState(defaultProyectos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  let proyectosBuscados = [];
+
+  if (!searchValue.length >= 1) {
+    proyectosBuscados = proyectos;
+  } else {
+    proyectosBuscados = proyectos.filter(proyecto => {
+      const proyectoNombre = proyecto.nombre.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return proyectoNombre.includes(searchText);
+    });
+  }
+
   return (
     <AppUI
-    proyectosTotal={defaultProyectos}
+      proyectosTotal={defaultProyectos}
+      proyectosBuscados={proyectosBuscados}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
     />
   );
 }
--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import {ListaProyectoOperativo} from '../Components/listaProyectoOperativo/listaProyectoOperativo.js';
-import {ProyectoOperativo} from '../Components/proyectoOperativo/proyectoOperativo.js';
-import {BusquedaProyecto} from '../Components/BusquedaProyecto/BusquedaProyecto.js';
+import React from "react";
+import { ListaProyectoOperativo } from "../Components/listaProyectoOperativo/listaProyectoOperativo.js";
+import { ProyectoOperativo } from "../Components/proyectoOperativo/proyectoOperativo.js";
+import { BusquedaProyecto } from "../Components/BusquedaProyecto/BusquedaProyecto.js";
 
+function AppUI({ proyectosTotal, searchValue, setSearchValue,proyectosBuscados }) {
+  return (
+    <React.Fragment>
+      <BusquedaProyecto
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
-function AppUI({
-    proyectosTotal,
-  }) 
-  {
-    return (
-      <React.Fragment>
-         <BusquedaProyecto/>
+      <ListaProyectoOperativo>
+        {proyectosBuscados.map((proyecto) => (
+          <ProyectoOperativo
+            key={proyecto.nombre}
+            nombre={proyecto.nombre}
+            fechaInicio={proyecto.fechaInicio}
+            fechaFin={proyecto.fechaFin}
+            fechaCreación={proyecto.fechaCreacion}
+            estado={proyecto.estado}
+            descripción={proyecto.descripción}
+          />
+        ))}
+      </ListaProyectoOperativo>
+    </React.Fragment>
+  );
+}
 
-        <ListaProyectoOperativo>
-          {proyectosTotal.map(proyecto => (
-            <ProyectoOperativo
-              key={proyecto.nombre}
-              nombre={proyecto.nombre}
-              fechaInicio={proyecto.fechaInicio}
-              fechaFin={proyecto.fechaFin}
-              fechaCreación={proyecto.fechaCreacion}
-              estado={proyecto.estado}
-              descripción= {proyecto.descripción}
-              />
-          ))}
-        </ListaProyectoOperativo>  
-      </React.Fragment>
-    );
-  }
-  
-  export { AppUI };
+export { AppUI };
