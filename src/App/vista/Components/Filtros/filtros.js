19206745aa8a247019b9e5f5b405564159764d97
wildchamo <wildchamo@gmail.com>
2022-10-09 14:51:17 -0500

    agregamos imagenes y generamos estructura a los filtros

--- src/App/vista/Components/Filtros/filtros.js
+++ src/App/vista/Components/Filtros/filtros.js
@@ -1,15 +1,42 @@
 import React from "react";
 import "./filtros.css";
 import { POContext } from "../../../controlador/controlador";
+import filtro from "./filtro.png";
+import arriba from "./vectorArriba.png";
+import abajo from "./vectorAbajo.png";
 
 function Filtros() {
   const {} = React.useContext(POContext);
   return (
     <div className="tituloListas">
-      <p>Nombre</p>
-      <p>Fecha de inicio</p>
-      <p>Fecha de finalización</p>
-      <p>Estado</p>
+      <div>
+        <p>Nombre</p>
+        <div className="flechas">
+          <img className="flecha" src={arriba} alt="Logo" />
+          <img className="flecha" src={abajo} alt="Logo" />
+        </div>
+      </div>
+
+      <div>
+        <p>Fecha de inicio</p>
+        <div className="flechas">
+          <img className="flecha" src={arriba} alt="Logo" />
+          <img className="flecha" src={abajo} alt="Logo" />
+        </div>
+      </div>
+
+      <div>
+        <p>fecha de finalización</p>
+        <div className="flechas">
+          <img className="flecha" src={arriba} alt="Logo" />
+          <img className="flecha" src={abajo} alt="Logo" />
+        </div>
+      </div>
+
+      <div>
+        <p>Estado</p> <img className="filtro" src={filtro} alt="Logo" />
+      </div>
+      
       <p>Editar</p>
       <p>Ver más</p>
       <p>Anular</p>
