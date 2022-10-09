851f841cc72d3e3ae54528372f94b1b169b95c49
wildchamo <wildchamo@gmail.com>
2022-10-09 04:14:51 -0500

    importamos el provider desde app.js

--- src/App/controlador/App.js
+++ src/App/controlador/App.js
@@ -1,43 +1,19 @@
 import React from "react";
 import { AppUI } from "../vista/AppUI.js";
-
-const defaultProyectos = [
-  {
-    nombre: "ChatBot Sinapsis (Diseño de flujo)",
-    fechaInicio: "2021-11-03",
-    fechaFin: "2022-10-02",
-    fechaCreacion: "2021-11-03",
-    estado: "Activo",
-    descripcion: "Soy una descripción",
-  },
-  {
-    nombre: "Investigación arquitectura PWA",
-    fechaInicio: "2021-11-03",
-    fechaFin: "2022-10-02",
-    fechaCreacion: "",
-    estado: "Cerrado",
-    descripcion: "Soy una descripción",
-  },
-  {
-    nombre: "Software de encuestas Limesurvey",
-    fechaInicio: "2021-11-03",
-    fechaFin: "2022-10-02",
-    fechaCreacion: "",
-    estado: "Cancelado",
-    descripcion: "Soy una descripción",
-  },
-];
+import {POProvider} from "./controlador";
 
 function App() {
 
 
   return (
-    <AppUI
-      proyectosTotal={defaultProyectos}
-      proyectosBuscados={proyectosBuscados}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-    />
+    <POProvider>
+      <AppUI
+        proyectosTotal={defaultProyectos}
+        proyectosBuscados={proyectosBuscados}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+    </POProvider>
   );
 }
 
---
+++ src/App/controlador/controlador.js
@@ -0,0 +1,61 @@
+import React, { createContext } from "react";
+
+const defaultProyectos = [
+    {
+      nombre: "ChatBot Sinapsis (Diseño de flujo)",
+      fechaInicio: "2021-11-03",
+      fechaFin: "2022-10-02",
+      fechaCreacion: "2021-11-03",
+      estado: "Activo",
+      descripcion: "Soy una descripción",
+    },
+    {
+      nombre: "Investigación arquitectura PWA",
+      fechaInicio: "2021-11-03",
+      fechaFin: "2022-10-02",
+      fechaCreacion: "",
+      estado: "Cerrado",
+      descripcion: "Soy una descripción",
+    },
+    {
+      nombre: "Software de encuestas Limesurvey",
+      fechaInicio: "2021-11-03",
+      fechaFin: "2022-10-02",
+      fechaCreacion: "",
+      estado: "Cancelado",
+      descripcion: "Soy una descripción",
+    },
+  ];
+
+const POContext= React.createContext();
+
+function POProvider(props){
+    const [proyectos, setProyectos] = React.useState(defaultProyectos);
+    const [searchValue, setSearchValue] = React.useState("");
+  
+    let proyectosBuscados = [];
+    let proyectosTotal= defaultProyectos.length;
+  
+    if (!searchValue.length >= 1) {
+      proyectosBuscados = proyectos;
+    } else {
+      proyectosBuscados = proyectos.filter(proyecto => {
+        const proyectoNombre = proyecto.nombre.toLowerCase();
+        const searchText = searchValue.toLowerCase();
+        return proyectoNombre.includes(searchText);
+      });
+    }
+    return(
+        <POContext.Provider value={{
+            proyectosTotal,
+            proyectosBuscados,
+            searchValue,
+            setSearchValue
+        }
+        }>
+            {props.children}
+        </POContext.Provider>
+    )
+}
+
+export {POContext,POProvider}
