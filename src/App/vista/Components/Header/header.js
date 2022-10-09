7538a4f158e7a375c37005e467e42a9668bf58e8
wildchamo <wildchamo@gmail.com>
2022-10-09 13:34:05 -0500

    creación componente header para mejor semantica

---
+++ src/App/vista/Components/Header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BusquedaProyecto } from "../BusquedaProyecto/BusquedaProyecto";
+import "./header.css";
+
+function Header(){
+    return(
+        <header>
+            <BusquedaProyecto/>
+        </header>
+    
+    )
+}
+export{Header};
