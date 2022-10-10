8e93924f462530fcea841bf642ac13b440e9521e
wildchamo <wildchamo@gmail.com>
2022-10-10 11:43:07 -0500

    eventos en PO

--- src/App/vista/Components/proyectoOperativo/proyectoOperativo.js
+++ src/App/vista/Components/proyectoOperativo/proyectoOperativo.js
@@ -6,20 +6,26 @@ import ver from './ver.png';
 
 function ProyectoOperativo(props) {
 
-
-  const onDelete =() =>{
+  const editarPO =() =>{
+    alert("Borraste el todo " + props.text);
+  }
+  const verMasPO =() =>{
+    alert("Borraste el todo " + props.text);
+  }
+  const anularPO =() =>{
     alert("Borraste el todo " + props.text);
   }
 
+  
   return (
     <li className="proyectoOperativo">
       <p>{props.nombre}</p>
       <p>{props.fechaInicio}</p>
       <p>{props.fechaFin}</p>
       <p>● {props.estado}</p>
-      <p><img src={editar} alt="botón editar" /></p>
-      <p><img src={ver} alt="botón ver más" /></p>
-      <p><img src={anular} alt="botón anular" /></p>
+      <p onClick={editarPO}><img src={editar} alt="botón editar" /></p>
+      <p onClick={verMasPO}><img src={ver} alt="botón ver más" /></p>
+      <p onClick={anularPO}><img src={anular} alt="botón anular" /></p>
     
     </li>
   );
