b8a4e548b668b66c16875b83e70dc317b04eb371
wildchamo <wildchamo@gmail.com>
2022-10-10 14:42:56 -0500

    base maquetación

--- src/App/vista/Components/formularioPO/formularioPO.js
+++ src/App/vista/Components/formularioPO/formularioPO.js
@@ -28,9 +28,25 @@ function FormularioPO() {
         onChange={onChange}
         placeholder="Nombre del proyecto"
       ></textarea>
-      {/* 
-      <label>Descripción proyecto</label>
-      <textarea placeholder="Descripción del proyecto"></textarea> */}
+      <div>
+        <label>Fecha de inicio</label>
+        <textarea placeholder="fecha de inicio"></textarea>
+        <label>Fecha de finalización</label>
+        <textarea placeholder="fecha de finalización"></textarea>
+        <label>Fecha de creación</label>
+        <textarea placeholder="fecha de creación" readOnly>
+          11-10-2022
+        </textarea>
+        <label>Estado proyecto</label>
+        <select placeholder="estado">
+          <option value="r">Rojo</option>
+          <option value="a">Azul</option>
+          <option value="v">Verde</option>
+        </select>
+      </div>
+
+      <label>descripcion del proyecto</label>
+      <textarea placeholder=""></textarea>
       <div>
         <p>Los campos marcados con ‘*’ son de carácter obligatorio.</p>
         <button type="button" onClick={onCancel}>
