7d45d910753a48f92b7b4616388a9672bc8a319e
GABRIELA PERALTA <gabrielaperaltadelavilla@hotmail.com>
2022-10-10 01:33:59 -0500

    header

--- src/App/vista/Components/Header/header.js
+++ src/App/vista/Components/Header/header.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { BusquedaProyecto } from "../BusquedaProyecto/BusquedaProyecto";
 import "./header.css";
+import logoSencillo from "./logosencillo.png";
+import campana from "./campana.png";
+import foto from "./foto.png";
+import salir from "./salir.png";
+
 
 function Header(){
     return(
         <header class="box">
-            <img></img>
+            <div className="logoSencillo">
+             <img className="logoSencillo" src={logoSencillo} alt="Logo" />
+            </div>
             <h1>
                 ¡Bienvenido!
             </h1>
             <BusquedaProyecto/>
+            <div className="campana">
+                <img className="campana" src={campana} alt="Campana para notificación" />
+            </div>
+            <div className="foto">
+                <img className="foto" src={foto} alt="Foto de usuario" />
+            </div>
+            <div className="salir">
+                <img className="salir" src={salir} alt="Cerrar sesión" />
+            </div>            
         </header>
     
     )
