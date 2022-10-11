110570294af9be867b6ed30d8d75a4fe01a49e33
wildchamo <wildchamo@gmail.com>
2022-10-11 15:13:34 -0500

    cargo los datos al formulario de editar

--- src/App/vista/Components/formularioPOedit/formularioPOedit.js
+++ src/App/vista/Components/formularioPOedit/formularioPOedit.js
@@ -2,8 +2,8 @@ import React from "react";
 import { POContext } from "../../../controlador/controlador";
 import "./formularioPOEdit.css";
 
-function FormularioPOedit(props) {
-  const { setOpenModalEditar } = React.useContext(POContext);
+function FormularioPOedit() {
+  const { setOpenModalEditar, proyectoSelec } = React.useContext(POContext);
   const onCancel = () => {
     setOpenModalEditar(false);
   };
@@ -11,20 +11,20 @@ function FormularioPOedit(props) {
     <form className="FormularioPOedit">
       <h2>Editando proyecto</h2>
       <label>Nombre del proyecto</label>
-      <textarea maxLength={50} value={props.nombre}></textarea>
+      <textarea maxLength={50} value={proyectoSelec.nombre}></textarea>
       <div className="divisor">
         <label>Fecha de inicio</label>
-        <textarea value={props.fechaInicio}></textarea>
+        <textarea value={proyectoSelec.fechaInicio}></textarea>
         <label>Fecha de finalización</label>
-        <textarea value={props.fechaFin}></textarea>
+        <textarea value={proyectoSelec.fechaFin}></textarea>
         <label>Fecha de creación</label>
-        <textarea value={props.fechaCreacion}></textarea>
+        <textarea value={proyectoSelec.fechaCreacion}></textarea>
         <label>Estado proyecto</label>
-        <textarea value={props.estado}></textarea>
+        <textarea value={proyectoSelec.estado}></textarea>
       </div>
 
       <label>descripcion del proyecto</label>
-      <textarea  maxLength={1000} value={props.descripcion}></textarea>
+      <textarea  maxLength={1000} value={proyectoSelec.descripcion}></textarea>
       <button onClick={onCancel}>Volver</button>
     </form>
   );
