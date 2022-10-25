cdb7b12a9d15988e0db26d3cfac9f1f5a18817cd
wildchamo <wildchamo@gmail.com>
2022-10-24 20:22:52 -0500

    base maquetación login

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import { AppUI } from "./vista/vista.js";
 import { POProvider } from "./controlador/controlador";
+import { Login } from "./vista/Components/login/login.js";
 
 function App() {
   return (
-    <POProvider>
-      <AppUI />
-    </POProvider>
+    <BrowserRouter>
+      <POProvider>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route path="/dashboard" element={<AppUI />}></Route>
+        </Routes>
+      </POProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/App/vista/Components/login/login.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import logouao from "./logouao.png";
+import opmanager from "./opmanager.png";
+import trabajo from "./trabajo.png";
+import axios from "axios";
+import "./login.css";
+
+const Login = () => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+
+  //   const history = useHistory();
+
+  //   const Auth = async (e) => {
+  //     e.preventDefault();
+  //     try {
+  //       await axios.post("http://localhost:5000/login", {
+  //         email: email,
+  //         password: password,
+  //       });
+  //       history.push("/dashboard");
+  //     } catch (error) {
+  //       if (error.response) {
+  //         setMsg(error.response.data.msg);
+  //       }
+  //     }
+  //   };
+  {
+    /*onSubmit={Auth}*/
+  }
+
+  return (
+    <React.Fragment>
+      <div className="contenedorLogin">
+        <div>
+          <img src={opmanager} alt="Logo" />
+        </div>
+
+        <form>
+          <label className="label">Nombre de usuario</label>
+          <div>
+            <input
+              type="text"
+              className="input"
+              placeholder="Nombre de usuario"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+          </div>
+
+          <label className="label">Contraseña</label>
+          <div>
+            <input
+              type="password"
+              className="input"
+              placeholder="Contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button>Iniciar sesión</button>
+        </form>
+        <div>
+          <img src={logouao} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export { Login };
