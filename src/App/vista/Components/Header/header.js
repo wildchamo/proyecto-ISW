4a3022bbbb9d93ec68a29ad6f06c08638264ff84
wildchamo <wildchamo@gmail.com>
2022-10-24 20:56:15 -0500

    login y logout hechos

--- src/App/vista/Components/Header/header.js
+++ src/App/vista/Components/Header/header.js
@@ -6,32 +6,42 @@ import campana from "./campana.png";
 import foto from "./foto.png";
 import salir from "./salir.png";
 import { POContext } from "../../../controlador/controlador";
+import { useNavigate } from "react-router-dom";
 
+function Header() {
+  const history = useNavigate();
+  const { jefeUnidad } = React.useContext(POContext);
 
-function Header(){
-    const { jefeUnidad } =
-    React.useContext(POContext);
+  const logout = async (e) => {
+    history("/");
+  };
 
-    return(
-        <header className="box">
-            <div className="logoSencillo">
-             <img className="logoSencillo" src={logoSencillo} alt="Logo" />
-            </div>
-            <h1>
-                ¡Bienvenido, {jefeUnidad} !
-            </h1>
-            <BusquedaProyecto/>
-            <div className="campana">
-                <img className="campana" src={campana} alt="Campana para notificación" />
-            </div>
-            <div className="foto">
-                <img className="foto" src={foto} alt="Foto de usuario" />
-            </div>
-            <div className="salir">
-                <img className="salir" src={salir} alt="Cerrar sesión" />
-            </div>            
-        </header>
-    
-    )
+  return (
+    <header className="box">
+      <div className="logoSencillo">
+        <img className="logoSencillo" src={logoSencillo} alt="Logo" />
+      </div>
+      <h1>¡Bienvenido, {jefeUnidad} !</h1>
+      <BusquedaProyecto />
+      <div className="campana">
+        <img
+          className="campana"
+          src={campana}
+          alt="Campana para notificación"
+        />
+      </div>
+      <div className="foto">
+        <img className="foto" src={foto} alt="Foto de usuario" />
+      </div>
+      <div className="salir">
+        <img
+          className="salir"
+          src={salir}
+          alt="Cerrar sesión"
+          onClick={logout}
+        />
+      </div>
+    </header>
+  );
 }
-export{Header};
+export { Header };
