435ac55ab7489324235ec21bac32c7310ee9e4eb
wildchamo <wildchamo@gmail.com>
2022-10-26 18:26:48 -0500

    conexion 2, se genera una inserción

--- src/App/modelo/index.js
+++ src/App/modelo/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "ISW",
+});
 
 app.get("/", (req, res) => {
-  res.send("hola");
+  const sqlInsert =
+    "INSERT INTO administrador (nombre,correo,contraseña) VALUES ('jose','jose@uao.co', '1234');";
+
+    db.query(sqlInsert, (req, result)=>{
+    res.send("se insertó");
+    })
 });
 
 app.listen(3001, () => {
