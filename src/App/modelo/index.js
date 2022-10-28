6431c13ace0f3d91fdc32e9cd07e3e5b3adff8ae
Duvan Espitia <duvan.espitia@uao.edu.co>
2022-10-27 22:22:02 -0500

    dependencias

--- src/App/modelo/index.js
+++ src/App/modelo/index.js
@@ -8,18 +8,18 @@ app.use(cors());
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "password",
-  database: "ISW",
+  password: "root",
+  database: "conexion_bd",
 });
 app.get("/api/get/es", (req, res) => {
-  const sqlSelect = "SELECT * FROM ISW.estado where not nombre='Anulado';"
+  const sqlSelect = "SELECT * FROM conexion_bd.estado where not nombre='Anulado';"
   db.query(sqlSelect, (err, result) => {
     res.send(result);
   });
 });
 
 app.get("/api/get", (req, res) => {
-  const sqlSelect = "call ISW.Consultar_Proyectos_Por_Unidad(?);";
+  const sqlSelect = "call conexion_bd.Consultar_Proyectos_Por_Unidad(?);";
   db.query(sqlSelect, ["amunoz"], (err, result) => {
     res.send(result);
   });
