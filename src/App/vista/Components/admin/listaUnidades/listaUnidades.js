5d098d08cfbdd66292b46ed09f202fa98c32d355
wildchamo <wildchamo@gmail.com>
2022-11-16 17:40:51 -0500

    cambios a estilos para que tenga mas congruencia

--- src/App/vista/Components/admin/listaUnidades/listaUnidades.js
+++ src/App/vista/Components/admin/listaUnidades/listaUnidades.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./listaUnidades.css";
 import { UOContext } from "../../../../controlador/controladorUnidades";
 import { BotonCrearUnidad } from "../BotonCrearUnidad/BotonCrearUnidad.js";
-import {BotonCrearEstado} from "../BotonCrearEstado/BotonCrearUnidad"
+import { BotonCrearEstado } from "../BotonCrearEstado/BotonCrearUnidad";
 function ListaUnidades(props) {
-  const {setOpenModalCrear,noUnidades,setOpenModalEstado} = React.useContext(UOContext);
+  const { setOpenModalCrear, noUnidades, setOpenModalEstado } =
+    React.useContext(UOContext);
 
   return (
     <section className="ListaUnidades">
       <h1>Unidades Operativas de la DTI</h1>
+      <div className="tituloU">
+        <p>Nombre unidad</p>
+        <p>Jefe unidad asociado</p>
+        <p>Editar</p>
+      </div>
 
       <ul>{props.children}</ul>
       <div className="footer">
